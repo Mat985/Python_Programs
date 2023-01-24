018fbd9d181d9b92bc3f1814f09abf529c89c697
Mat985 <matuspiacek@gmail.com>
2023-01-24 09:18:41 +0100

    Update higher number.py

--- Number comparison/higher number.py
+++ Number comparison/higher number.py
@@ -1,4 +1,13 @@
 numbers=[]
-for i in range (2):
-    numbers+=input ("Enter 2 numbers")
-print(max(numbers))
+try:
+    for i in range (2):
+        numbers+=[int (input ("Enter number "))]
+
+except ValueError:
+    print ("This is not a number with base 10")
+    numbers+=[int (input ("Enter number "))]
+
+if numbers [0]!=numbers[1]:
+    print(max(numbers))
+else :
+    print("Numers are equal there is no higher number")
